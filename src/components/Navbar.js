ce3835c7e2ecdc466795cad16d87b13c8dd607d4
fernandogonard <matiasgonard1@gmail.com>
2025-12-14 22:50:32 -0300

    tersecond commit del proyecto artístico

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  // Cierra el menú al hacer clic en un enlace
+  const handleLinkClick = () => setOpen(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <a className="navbar-logo" href="#top">Alberto Álvarez</a>
-        <ul className="navbar-links">
-          <li><a href="#biografia">Biografía</a></li>
-          <li><a href="#obra">Obra</a></li>
-          <li><a href="#prensa">Prensa</a></li>
-          <li><a href="#blog">Blog</a></li>
-          <li><a href="#contacto">Contacto</a></li>
+        <button
+          className={`navbar-burger${open ? ' open' : ''}`}
+          aria-label="Abrir menú"
+          aria-expanded={open}
+          onClick={() => setOpen(!open)}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <ul className={`navbar-links${open ? ' show' : ''}`}>
+          <li><a href="#biografia" onClick={handleLinkClick}>Biografía</a></li>
+          <li><a href="#obra" onClick={handleLinkClick}>Obra</a></li>
+          <li><a href="#prensa" onClick={handleLinkClick}>Prensa</a></li>
+          <li><a href="#blog" onClick={handleLinkClick}>Blog</a></li>
+          <li><a href="#contacto" onClick={handleLinkClick}>Contacto</a></li>
         </ul>
       </div>
     </nav>

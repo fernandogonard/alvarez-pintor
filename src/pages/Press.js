658b0ae474b7840b084cb59982b2009842f6822c
fernandogonard <matiasgonard1@gmail.com>
2025-12-15 00:14:28 -0300

    tersecond commit del proyecto artístico1.1

--- src/pages/Press.js
+++ src/pages/Press.js
@@ -13,17 +13,29 @@ function Press() {
         <li>
           <span>2024</span> – <a href="https://contrastemdp.com.ar" target="_blank" rel="noopener noreferrer">“Inauguran en el Puerto la muestra ‘ANDINO’ de Alberto Alvarez”</a> – Contraste MDP (05/09/2024)<br/>
           Artículo que anuncia la inauguración de la muestra Andino en el Teatro Auditorium de Mar del Plata. Se exhiben paisajes andinos pintados al óleo utilizando el pincel sostenido con la boca, en homenaje a los paisajes de Mendoza, tierra natal del artista. Se destaca que Álvarez es miembro pleno de la Asociación Mundial de Pintores con la Boca y el Pie (VDMFK), delegado por Argentina desde 2019, y se repasan sus premios más importantes: Premio Nacional Bienal A.L.P.I. (1977) y Premio Mundial de la Paz (2000).
-          <br/><img src="/assets/press/article1.png" alt="Contraste MDP - Andino" style={{maxWidth:'300px',marginTop:'10px'}}/>
+            <img src="/assets/press/articulo1.png" alt="Contraste MDP - Andino" />
+            <div>
+              <span>2024</span> – <a href="https://contrastemdp.com.ar" target="_blank" rel="noopener noreferrer">“Inauguran en el Puerto la muestra ‘ANDINO’ de Alberto Alvarez”</a> – Contraste MDP (05/09/2024)<br/>
+              Artículo que anuncia la inauguración de la muestra Andino en el Teatro Auditorium de Mar del Plata. Se exhiben paisajes andinos pintados al óleo utilizando el pincel sostenido con la boca, en homenaje a los paisajes de Mendoza, tierra natal del artista. Se destaca que Álvarez es miembro pleno de la Asociación Mundial de Pintores con la Boca y el Pie (VDMFK), delegado por Argentina desde 2019, y se repasan sus premios más importantes: Premio Nacional Bienal A.L.P.I. (1977) y Premio Mundial de la Paz (2000).
+            </div>
         </li>
         <li>
           <span>2024</span> – <a href="https://www.lacapitalmdp.com/obras-pintadas-con-el-pincel-en-la-boca-podran-verse-en-la-muestra-andino/#google_vignette" target="_blank" rel="noopener noreferrer">“Obras pintadas con el pincel en la boca podrán verse en la muestra ‘Andino’”</a> – Diario La Capital (Mar del Plata) (05/09/2024)<br/>
           Crónica del mismo evento expositivo. La muestra Andino rinde tributo a la naturaleza de los Andes mendocinos a través de obras al óleo pintadas con la boca. Se recuerda la trayectoria de Álvarez y sus premios.
-          <br/><img src="/assets/press/article2.png" alt="La Capital - Andino" style={{maxWidth:'300px',marginTop:'10px'}}/>
+            <img src="/assets/press/articulo2.png" alt="La Capital - Andino" />
+            <div>
+              <span>2024</span> – <a href="https://www.lacapitalmdp.com/obras-pintadas-con-el-pincel-en-la-boca-podran-verse-en-la-muestra-andino/#google_vignette" target="_blank" rel="noopener noreferrer">“Obras pintadas con el pincel en la boca podrán verse en la muestra ‘Andino’”</a> – Diario La Capital (Mar del Plata) (05/09/2024)<br/>
+              Crónica del mismo evento expositivo. La muestra Andino rinde tributo a la naturaleza de los Andes mendocinos a través de obras al óleo pintadas con la boca. Se recuerda la trayectoria de Álvarez y sus premios.
+            </div>
         </li>
         <li>
           <span>2023</span> – <a href="https://wwwold.municipiosanjuan.gob.ar" target="_blank" rel="noopener noreferrer">“Estas son las artistas ganadoras del ‘Salón Juego de Damas 2023’”</a> – Municipio de San Juan (05/07/2023)<br/>
           Comunicado de prensa sobre los ganadores del certamen artístico “Juego de Damas”. Incluye a Álvarez como jurado.
-          <br/><img src="/assets/press/article3.png" alt="San Juan - Juego de Damas" style={{maxWidth:'300px',marginTop:'10px'}}/>
+            <img src="/assets/press/articulo3.png" alt="San Juan - Juego de Damas" />
+            <div>
+              <span>2023</span> – <a href="https://wwwold.municipiosanjuan.gob.ar" target="_blank" rel="noopener noreferrer">“Estas son las artistas ganadoras del ‘Salón Juego de Damas 2023’”</a> – Municipio de San Juan (05/07/2023)<br/>
+              Comunicado de prensa sobre los ganadores del certamen artístico “Juego de Damas”. Incluye a Álvarez como jurado.
+            </div>
         </li>
         <li>
           <span>2021</span> – <a href="https://www.lavoz.com.ar/historias-de-vida/alberto-alvarez-la-nariz-y-la-boca-son-mis-manos-y-mis-brazos/" target="_blank" rel="noopener noreferrer">“La nariz y la boca son mis manos y mis brazos”</a> – La Voz del Interior<br/>
